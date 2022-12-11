a54c8864f8973570aaa9351dba1f9e3cbe7ee064
ewj327 <ewj327@bu.edu>
2022-12-11 11:31:02 -0500

    fixing minor migration issues
    ask me for the django secret key if you need it

--- djangoNR/restaurant/restaurant/settings.py
+++ djangoNR/restaurant/restaurant/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kbm!c%m%93$=o91t#+58-be)29umix0qj4(wbta(8i$nilh75d'
+SECRET_KEY = "SECRET"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
